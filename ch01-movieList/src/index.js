2736754647c95300f69b7aa23204c7a7b7e20b56
Dave Kanter <moatgoat@hotmail.com>
2020-08-01 15:06:05 -0700

    Beginning of chapter 2

---
+++ ch01-movieList/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <h1>Movie List</h1>
+      <nav className="navbar sticky-top navbar-light bg-dark">
+        <h1 className="navbar-brand text-light">Movie List</h1>
+      </nav>
+      <List />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
